ac5bf27fb0842cc99ce5648f34c1772983e26bd9
Stacy McCarrell <clovercodes7062@gmail.com>
2023-12-25 23:27:40 -0600

    localScreen is now dragable

---
+++ dragVideosScript.js
@@ -0,0 +1,30 @@
+function runDragVideosScript() {
+    let isDragging = false;
+    let offsetX, offsetY;
+
+    const screenShareContainer = document.getElementById('localScreenContainer');
+
+    screenShareContainer.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        offsetX = e.clientX - screenShareContainer.getBoundingClientRect().left;
+        offsetY = e.clientY - screenShareContainer.getBoundingClientRect().top;
+        screenShareContainer.style.cursor = 'grabbing';
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+
+        const x = e.clientX - offsetX;
+        const y = e.clientY - offsetY;
+
+        screenShareContainer.style.left = `${x}px`;
+        screenShareContainer.style.top = `${y}px`;
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+        screenShareContainer.style.cursor = 'grab';
+    });
+}
+
+runDragVideosScript();
