5cead9512b0a2bd3c6c878d1d950b9eae002272c
Stacy McCarrell <clovercodes7062@gmail.com>
2023-12-31 19:02:17 -0600

    fix bugs, added observerReceiveMsgList

---
+++ js/observeReceivedMsgList.js
@@ -0,0 +1,18 @@
+function runObserveReceivedMsgList() {
+    const messagesContainer = document.getElementById('messages-container');
+
+    const observer = new MutationObserver(() => {
+        const receievedMsgList = document.getElementById('receivedMsgList');
+
+        if (receievedMsgList) {
+            console.log('observer found mutation in messages-container, correcting receivedMsgList margin-top', receievedMsgList);
+            receievedMsgList.style.marginTop = `${90 * receievedMsgList.childElementCount}px`;
+        }
+    });
+
+    const config = { childList: true };
+
+    observer.observe(messagesContainer, config);
+}
+
+runObserveReceivedMsgList();
